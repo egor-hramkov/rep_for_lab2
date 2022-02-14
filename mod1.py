36b4b4a921dfc4936d10e7cd11c0fccae322ef12
egor-hramkov <egor_kuplinov@mail.ru>
2022-02-14 16:18:46 +0400

    finishing the task

--- mod1.py
+++ mod1.py
@@ -8,9 +8,10 @@ a = np.array(table)
 
 kkal = np.sort(a, axis=-1, kind='quicksort', order=['Energ_Kcal', 'Shrt_Desc'])[::-1]
 sugar = np.sort(a, axis=-1, kind='quicksort', order=['Sugar_Tot_g', 'Shrt_Desc'])
+protein = np.sort(a, axis=-1, kind='quicksort', order=['Protein_g'])[::-1]
+vit_c = np.sort(a, axis=-1, kind='quicksort', order=['Vit_C_mg'])[::-1]
 
-#protein = np.sort(a, axis=-1, kind='quicksort', order=['Protein_(g)'])[::-1]
-#vit_c = np.sort(a, axis=-1, kind='quicksort', order=['Vit_C_(mg)'])[::-1]
-
-
-print(sugar);
+print(kkal[0][1])
+print(sugar[0][1])
+print(protein[0][1])
+print(vit_c[0][1])
